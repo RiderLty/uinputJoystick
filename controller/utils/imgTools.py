8e3ae4e574be7e835d26a68e31ad72e9d182916d
DriverLin <lty6531600@gmail.com>
2024-03-14 16:09:30 +0800

    fix img tools

--- controller/utils/imgTools.py
+++ controller/utils/imgTools.py
@@ -6,8 +6,7 @@ import mss
 import numpy as np
 import requests
 from PIL import Image
-from cnocr.ppocr.utility import draw_ocr_box_txt
-
+# from cnocr.ppocr.utility import draw_ocr_box_txt
 
 def printPerformance(func: callable) -> callable:
     if asyncio.iscoroutinefunction(func):
@@ -28,79 +27,101 @@ def printPerformance(func: callable) -> callable:
     return wrapper
 
 
+
+
 def url2ImgNp(url):
     '''从URL下载图片，返回np格式的BGR图像'''
-    response = requests.get(url)
-    image_data = response.content
-    image_array = np.frombuffer(image_data, np.uint8)
+    image_array = np.frombuffer(requests.get(url).content, np.uint8)
     image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
     return image
 
 
 def url2ImgPIL(url):
-    response = requests.get(url)
-    image_data = response.content
-    img = Image.open(BytesIO(image_data)).convert("RGB")
-    return img
+    '''从URL下载图片，返回PIL的RGB图像'''
+    return Image.open(BytesIO(requests.get(url).content)).convert("RGB")
 
 
-def screenCapPIL():
-    with mss.mss() as m:
-        rect = (0, 0, 1920, 1080)
-        sc_img = m.grab(rect)
-        return Image.frombytes("RGB", sc_img.size, sc_img.bgra, "raw", "BGRX")
+sct = mss.mss()
 
+def mss2np(display_num=-1, zone=[(0, 0), (1920, 1080)]):
+    '''display_num 显示器编号  1,2,3...    -1为主显示器
 
-def screenCapNP():
-    with mss.mss() as m:
-        rect = (0, 0, 1920, 1080)
-        sc_img = m.grab(rect)
-        npImg = np.array(sc_img)
-        npImg = cv2.cvtColor(npImg, cv2.COLOR_BGRA2BGR)
-        return npImg
-        # result = np.frombuffer(sc_img.bgra, dtype=np.uint8)
-        # width = sc_img.size.width  # 宽度
-        # height = sc_img.size.height  # 高度
-        # result = cv2.cvtColor(result.reshape(
-        #     (height, width, 4)), cv2.COLOR_BGRA2BGR)
-        # return result
+    zone 抓取区域 [(x1,y1),(x2,y2)] 左上角到右下角
+    
+    返回np格式的BGR图像
+    '''
+    assert zone[0][0] < zone[1][0]
+    assert zone[0][1] < zone[1][1]
+    mon = sct.monitors[display_num]
+    monitor = {
+        "top": mon["top"] + zone[0][1],
+        "left": mon["left"] + zone[0][0],
+        "width": zone[1][0] - zone[0][0],  # 手动指定区域
+        "height": zone[1][1] - zone[0][1],
+    }
+    def getSCreen():
+        screen = sct.grab(monitor)
+        img = np.array(screen)
+        img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+        return img
+    return getSCreen
+
+def mss2pil(display_num=-1, zone=[(0, 0), (1920, 1080)]):
+    '''display_num 显示器编号  1,2,3...    -1为主显示器
+
+    zone 抓取区域 [(x1,y1),(x2,y2)] 左上角到右下角
+    
+    返回PIL的RGB图像
+    '''
+    assert zone[0][0] < zone[1][0]
+    assert zone[0][1] < zone[1][1]
+    mon = sct.monitors[display_num]
+    monitor = {
+        "top": mon["top"] + zone[0][1],
+        "left": mon["left"] + zone[0][0],
+        "width": zone[1][0] - zone[0][0],  # 手动指定区域
+        "height": zone[1][1] - zone[0][1],
+    }
+    def getSCreen():
+        screen = sct.grab(monitor)
+        img = Image.frombytes("RGB", screen.size, screen.bgra, "raw", "BGRX")
+        return img
+    return getSCreen
 
 
 def pil2np(PILImg: Image):
-    npImg = np.array(PILImg)
-    return npImg
+    return cv2.cvtColor(np.array(PILImg) , cv2.COLOR_RGB2BGR)
 
 
 def np2pil(npImg: np.ndarray):
-    pilImg = Image.fromarray(npImg)
-    return pilImg
+    return Image.fromarray(npImg)
 
 
-def drawOCR2np(rawImg, ocrResult, font_path , asOne = False):
-    '''OCR结果绘制，输入NP，返回NP'''
-    txts = []
-    scores = []
-    boxes = []
-    boxesInt = []
+# def drawOCR2np(rawImg, ocrResult, font_path , asOne = False):
+#     '''OCR结果绘制，输入NP，返回NP'''
+#     txts = []
+#     scores = []
+#     boxes = []
+#     boxesInt = []
     
-    for _out in ocrResult:
-        txts.append(_out['text'])
-        scores.append(_out['score'])
-        boxesInt.append(_out['position'].astype(np.int64))
-        boxes.append(_out['position'])
-    pilRaw = np2pil(rawImg)
-    pilRaw.show()
-    draw_img = draw_ocr_box_txt( pilRaw , boxes, txts, scores, drop_score=0.0, font_path=font_path)
+#     for _out in ocrResult:
+#         txts.append(_out['text'])
+#         scores.append(_out['score'])
+#         boxesInt.append(_out['position'].astype(np.int64))
+#         boxes.append(_out['position'])
+#     pilRaw = np2pil(rawImg)
+#     pilRaw.show()
+#     draw_img = draw_ocr_box_txt( pilRaw , boxes, txts, scores, drop_score=0.0, font_path=font_path)
     
-    drawImg = cv2.cvtColor(draw_img, cv2.COLOR_RGB2BGR)
-    width = pilRaw.width
-    rawDraw = drawImg[:, :width]
-    whiteDraw = drawImg[:, width:]
-    if asOne == False:
-        return rawDraw, whiteDraw
-    else:
-        cv2.fillPoly(rawDraw, boxesInt, (255, 255, 255))
-        return np.where(whiteDraw == [255,255,255] , rawDraw  , whiteDraw)
+#     drawImg = cv2.cvtColor(draw_img, cv2.COLOR_RGB2BGR)
+#     width = pilRaw.width
+#     rawDraw = drawImg[:, :width]
+#     whiteDraw = drawImg[:, width:]
+#     if asOne == False:
+#         return rawDraw, whiteDraw
+#     else:
+#         cv2.fillPoly(rawDraw, boxesInt, (255, 255, 255))
+#         return np.where(whiteDraw == [255,255,255] , rawDraw  , whiteDraw)
     
 masks = [
     # 来复活
@@ -159,14 +180,21 @@ def drawHandelScreen(screen):
 
 
 if __name__ == "__main__":
-    from cnocr import CnOcr
-    oi = CnOcr()
+    # from cnocr import CnOcr
+    # oi = CnOcr()
+    npcap = mss2np(zone=[(0,1) , (500,500)])
+    pilcap = mss2pil(zone=[(0,1) , (500,500)])
     while True:
         img = url2ImgNp("http://192.168.3.155:4443/screenraw")
+        # img = pil2np(img)
+        
+        # img = pilcap()
+        # img = pil2np(img)
         # img = screenCapNP()
         img = drawHandelScreen(img)
-        out = oi.ocr(img)
-        a  = drawOCR2np(img , out , r"C:\Windows\Fonts\msyhl.ttc", True )
-        cv2.imshow('draw', a)
+        # out = oi.ocr(img)
+        # a  = drawOCR2np(img , out , r"C:\Windows\Fonts\msyhl.ttc", True )
+        cv2.imshow('draw', img)
         if cv2.waitKey(1) == 27:
             break
+
